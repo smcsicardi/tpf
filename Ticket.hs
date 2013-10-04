fb86390fd3984914328bb0a5a27dac2b0615b97f
smcsicardi <smcsicardi@gmail.com>
2013-10-03 23:01:37 -0300

    Update Ticket.hs

--- Ticket.hs
+++ Ticket.hs
@@ -9,3 +9,7 @@ todosLosTicketsParaLaMismaSalaT :: [Ticket] -> Bool
   todosLosTicketsParaLaMismaSalaT [] = True
   todosLosTicketsParaLaMismaSalaT [_] = True
   todosLosTicketsParaLaMismaSalaT t:tt:ts = (sala t == sala tt) && todosLosTicketsParaLaMismaSalaT tt:ts
+
+cambiarSalaT :: [Ticket] -> Sala -> Sala -> [Ticket]
+  cambiarSalaT [] _ _ = []
+  
