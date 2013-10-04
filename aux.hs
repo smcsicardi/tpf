bf9c9e86cfabaaf793b95b0ab7bc81625bf308fa
jmelfers <esjuanma@hotmail.com>
2013-10-04 10:21:21 -0300

    Update aux.hs

--- aux.hs
+++ aux.hs
@@ -1,11 +1,7 @@
 -- 1era funcion aux
 sinRepetidos :: (Eq a) => [a] -> Bool
-sinRepetidos (x:xs) = distintoAtodos x xs && sinRepetidos xs
+sinRepetidos (x:xs) = not elem x xs && sinRepetidos xs
 sinRepetidos _ = True
-
-distintoAtodos :: (Eq a) => a -> [a] -> Bool
-distintoAtodos x (y:xs) = not (x == y) && distintoAtodos x xs
-distintoAtodos _ _ = True
 
 -- 2da funcion aux
 listaOrdenada :: (Ord a) => [a] -> Bool
