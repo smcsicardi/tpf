7e20c928c0d3916857caeabf6d3f4826f10816fe
gushuro <gushurovich@gmail.com>
2013-10-06 23:28:23 -0300

    Update Cine.hs

--- Cine.hs
+++ Cine.hs
@@ -111,7 +111,7 @@ agregaEspectadorASala :: Cine -> Sala -> Cine
 agregaEspectadorASala (SalaSinPelicula c sa) s = SalaSinPelicula (agregaEspectadorASala c s) sa
 agregaEspectadorASala (SalaConPelicula c sa p e) s
 	|sa == s = SalaConPelicula c sa p (e+1)
-	|otherwise SalaConPelicula (agregaEspectadorASala c s) sa p e
+	|otherwise = SalaConPelicula (agregaEspectadorASala c s) sa p e
 agregaEspectadorASala (TicketVendido c t) = TicketVendido (agregaEspectadorASala c s) t
 
 pasarA3DUnaPeliculaC :: Cine -> Nombre -> (Cine,Pelicula)
